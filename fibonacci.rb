ddd8216006f085f656a6d6aab5045a7eea95eddd
carojane <carojane@gmail.com>
2014-09-09 11:16:20 -0400

    init commit

---
+++ fibonacci.rb
@@ -0,0 +1,10 @@
+def fib(digit, array = [0, 1])
+  array = array
+  until array.length == digit
+    next_digit = array[-1] + array[-2]
+    array << next_digit
+  end
+  array
+end
+
+puts fib(500)
