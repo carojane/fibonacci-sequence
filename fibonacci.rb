f4766e4472ef73ed4b6fbcb73a9dd7012b79821f
Carly Pulver <carojane@gmail.com>
2014-10-16 13:34:21 -0400

    Update fibonacci.rb

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,6 +1,6 @@
-def fib(digit, array = [1, 1])
-  array = array
-  until array.length == digit
+def fib(n)
+  array = [0, 1]
+  until array.length == n
     next_digit = array[-1] + array[-2]
     array << next_digit
   end
